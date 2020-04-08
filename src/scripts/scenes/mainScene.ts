abb277d9abe3946b74917f3c2c996352484c8414
Matthew Novosel <matthewnovosel@Matthews-MacBook-Air.local>
2020-04-07 23:37:35 -0400

    different graphics plus attack

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,10 +2,12 @@ import ExampleObject from '../objects/exampleObject';
 
 
 export default class MainScene extends Phaser.Scene {
-  private player: Phaser.GameObjects.Image;
+  private player;
   private background: Phaser.GameObjects.TileSprite;
   private words: string;
+  private health: integer;
   private wordLabel: Phaser.GameObjects.BitmapText;
+  private healthLabel: Phaser.GameObjects.BitmapText;
 
   constructor() {
     super({ key: 'MainScene' });
@@ -15,8 +17,13 @@ export default class MainScene extends Phaser.Scene {
     this.background = this.add.tileSprite(0, 0, this.scale.width, this.scale.height, "background");
     this.background.setOrigin(0, 0);
 
-    this.player = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2 - 20, "standing");
+    //this.player = this.add.image(this.scale.width / 2 - 50, this.scale.height / 2 - 20, "standing");
+    this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "idle");
+    this.player.play("idle_anim");
 
+
+
+    this.player.setScale(2, 2);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.BACKSPACE);
     for(let i : number = Phaser.Input.Keyboard.KeyCodes.A; i <= Phaser.Input.Keyboard.KeyCodes.Z; i++) {
@@ -24,14 +31,19 @@ export default class MainScene extends Phaser.Scene {
     }
 
     this.words = "";
-
+    this.health = 100;
     this.wordLabel = this.add.bitmapText(10, 5, "pixelFont", "Command", 16);
+    this.healthLabel = this.add.bitmapText(this.scale.width - 75, 5, "pixelFont", "health", 16);
   }
 
   update() {
+    //this.player.play("idle_anim");
+
     this.wordLabel.text = "Command:    " + this.words;
+    this.healthLabel.text = "Health: " + this.health;
     this.addLetters();
     this.movePlayer();
+   // this.player.play("idle_anim");
   }
 
   addLetters() {
@@ -60,10 +72,21 @@ export default class MainScene extends Phaser.Scene {
     if (this.words == "move left") {
       this.player.x -= 25;
       this.words = "";
+      this.player.play("idle_anim");
     }
 
     if (this.words == "move right") {
       this.player.x += 25;
+      this.words = "";
+      this.player.play("idle_anim");
+    }
+
+    if(this.words == "attack"){
+      //this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "idle");
+      this.player.play("attack_anim");
+      
+      //this.player.play("idle_anim");
+      //AnimationTimeline
       this.words = "";
     }
   }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,13 +4,39 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("standing", "assets/images/Stand.png");
-    this.load.image("background", "assets/images/bg.png");
+    this.load.image("standing", "assets/images/standing.png");
+    this.load.image("background", "assets/images/desert.png");
+    
     this.load.bitmapFont("pixelFont", "assets/fonts/font.png", "assets/fonts/font.xml");
+
+    this.load.spritesheet("attack", "assets/spritesheets/attack.png",{
+      frameWidth: 50,
+      frameHeight: 35
+    });
+    
+    this.load.spritesheet("idle", "assets/spritesheets/idle.png",{
+      frameWidth: 50,
+      frameHeight: 35
+    });
 
   }
 
   create() {
     this.scene.start('MainScene');
+     
+
+    this.anims.create({
+      key: "attack_anim",
+      frames: this.anims.generateFrameNumbers("attack", { start: 0, end: 4}),
+      frameRate: 20,
+      repeat: 0
+    });
+
+    this.anims.create({
+      key: "idle_anim",
+      frames: this.anims.generateFrameNumbers("idle", { start: 0, end: 1}),
+      frameRate: 5,
+      repeat: -1
+    });
   }
 }
