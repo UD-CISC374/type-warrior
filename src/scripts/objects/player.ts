583e52f23704a6ca6ecee567e63d2cbca2b27b5a
wrembish <wrembish@udel.edu>
2020-04-08 01:11:43 -0400

    set health in player object

--- src/scripts/objects/player.ts
+++ src/scripts/objects/player.ts
@@ -4,6 +4,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene : Phaser.Scene, x : number, y : number) {
         super(scene, x, y, "idle");
         this.play("idle_anim");
+        this.health = 100;
         scene.add.existing(this);
 
     }
