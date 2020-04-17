d477c5c0205d021f0d10a963d6723b92f10edb15
Matthew Novosel <matthewnovosel@Matthews-MacBook-Air.local>
2020-04-16 22:50:33 -0400

    Updates

---
+++ alpha/precache-manifest.e6c6691f21134bb94631083bd44251c7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/educational-phaser-game-template/alphaassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/educational-phaser-game-template/alphaassets/fonts/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/educational-phaser-game-template/alphaassets/fonts/font.xml"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-phaser-game-template/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-phaser-game-template/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-phaser-game-template/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-phaser-game-template/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "5c404bcb16c38c324b756229cf0af98e",
+    "url": "/educational-phaser-game-template/alphaassets/images/Stand.png"
+  },
+  {
+    "revision": "d59d04ef8525a3eec6a960ff85d8532c",
+    "url": "/educational-phaser-game-template/alphaassets/images/bg.png"
+  },
+  {
+    "revision": "16e1b08de14f9049abb2d62ed001d28e",
+    "url": "/educational-phaser-game-template/alphaassets/images/desert.png"
+  },
+  {
+    "revision": "f00b0e14a61c209d8ae5ab05a9a07dc3",
+    "url": "/educational-phaser-game-template/alphaassets/images/shop_background.jpg"
+  },
+  {
+    "revision": "4b0b9dd62716b5d8510917d503cdb0cd",
+    "url": "/educational-phaser-game-template/alphaassets/images/standing.png"
+  },
+  {
+    "revision": "528c6775a54486859aa17d71525a2bed",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/Minotaur - Sprite Sheet.png"
+  },
+  {
+    "revision": "4f6b0b46717a7913ad3e272733107f11",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/adventurer-hand-combat-Sheet.png"
+  },
+  {
+    "revision": "c363b13c7ba5b60c6d9e1bde3213afe3",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/adventurer-v1.5-Sheet.png"
+  },
+  {
+    "revision": "652e9f53d2b03843b97b2658d78c9852",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/demon-attack.png"
+  },
+  {
+    "revision": "ec00c433e1e9ce9f0be7c3e828b85c01",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/demon-idle.png"
+  },
+  {
+    "revision": "be6625892b3fdc2107673ef6d08f9dbe",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/demon-run.png"
+  },
+  {
+    "revision": "cc3cd02006ea4d35ec01ed21fefc2b2f",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/idle.png"
+  },
+  {
+    "revision": "daa48b77055640406269d95d6826bd0d",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/knight-attack.png"
+  },
+  {
+    "revision": "41e1f83588462b7d6cc5354a92652e4d",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/knight-idle.png"
+  },
+  {
+    "revision": "9d9c9481e0f99bacdb0de4dcdd0d2ec7",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/knight-sprite-sheet.png"
+  },
+  {
+    "revision": "c48885871ae6d8e4ecf105b30d4f3fe3",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/magic_cast.png"
+  },
+  {
+    "revision": "605c57a77e3f51c1aeda5a8e27836e96",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/punch.png"
+  },
+  {
+    "revision": "9b5298d7317a61bc09048f434b5f39bb",
+    "url": "/educational-phaser-game-template/alphaassets/spritesheets/sword-attack.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-phaser-game-template/alphafavicon.ico"
+  },
+  {
+    "revision": "b4a2594ea7ea4070571e",
+    "url": "/educational-phaser-game-template/alphagen.main.558d9f2210b713229287.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-phaser-game-template/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "f2347b314cf935015558aa9a1318177a",
+    "url": "/educational-phaser-game-template/alphaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-phaser-game-template/alphamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/educational-phaser-game-template/alphasw.js"
+  }
+]);
