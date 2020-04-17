8260fa9949f118bfc1cf3c9b45daa5bab45f1253
Matthew Novosel <matthewnovosel@Matthews-MacBook-Air.local>
2020-04-16 22:59:43 -0400

    Updates

---
+++ precache-manifest.da3bb3f7024ac13a0694c4a46e6097c0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/fonts/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "assets/fonts/font.xml"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "assets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "assets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "5c404bcb16c38c324b756229cf0af98e",
+    "url": "assets/images/Stand.png"
+  },
+  {
+    "revision": "d59d04ef8525a3eec6a960ff85d8532c",
+    "url": "assets/images/bg.png"
+  },
+  {
+    "revision": "16e1b08de14f9049abb2d62ed001d28e",
+    "url": "assets/images/desert.png"
+  },
+  {
+    "revision": "f00b0e14a61c209d8ae5ab05a9a07dc3",
+    "url": "assets/images/shop_background.jpg"
+  },
+  {
+    "revision": "4b0b9dd62716b5d8510917d503cdb0cd",
+    "url": "assets/images/standing.png"
+  },
+  {
+    "revision": "528c6775a54486859aa17d71525a2bed",
+    "url": "assets/spritesheets/Minotaur - Sprite Sheet.png"
+  },
+  {
+    "revision": "4f6b0b46717a7913ad3e272733107f11",
+    "url": "assets/spritesheets/adventurer-hand-combat-Sheet.png"
+  },
+  {
+    "revision": "c363b13c7ba5b60c6d9e1bde3213afe3",
+    "url": "assets/spritesheets/adventurer-v1.5-Sheet.png"
+  },
+  {
+    "revision": "652e9f53d2b03843b97b2658d78c9852",
+    "url": "assets/spritesheets/demon-attack.png"
+  },
+  {
+    "revision": "ec00c433e1e9ce9f0be7c3e828b85c01",
+    "url": "assets/spritesheets/demon-idle.png"
+  },
+  {
+    "revision": "be6625892b3fdc2107673ef6d08f9dbe",
+    "url": "assets/spritesheets/demon-run.png"
+  },
+  {
+    "revision": "cc3cd02006ea4d35ec01ed21fefc2b2f",
+    "url": "assets/spritesheets/idle.png"
+  },
+  {
+    "revision": "daa48b77055640406269d95d6826bd0d",
+    "url": "assets/spritesheets/knight-attack.png"
+  },
+  {
+    "revision": "41e1f83588462b7d6cc5354a92652e4d",
+    "url": "assets/spritesheets/knight-idle.png"
+  },
+  {
+    "revision": "9d9c9481e0f99bacdb0de4dcdd0d2ec7",
+    "url": "assets/spritesheets/knight-sprite-sheet.png"
+  },
+  {
+    "revision": "c48885871ae6d8e4ecf105b30d4f3fe3",
+    "url": "assets/spritesheets/magic_cast.png"
+  },
+  {
+    "revision": "605c57a77e3f51c1aeda5a8e27836e96",
+    "url": "assets/spritesheets/punch.png"
+  },
+  {
+    "revision": "9b5298d7317a61bc09048f434b5f39bb",
+    "url": "assets/spritesheets/sword-attack.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "9355fd9e7372bd3a3672f8b11efc1181",
+    "url": "index.html"
+  },
+  {
+    "revision": "08ac5a3a0dc77d442b09",
+    "url": "main.ad70f1cb8543e03a51ff.bundle.js"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "sw.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "vendors.0805bb80903235d40eaf.bundle.js"
+  }
+]);
